98b410c1c8862894923b1d8224ec7b7ba62ac69f
shamhasan <ihtishamulhasaaan@gmail.com>
2026-01-05 07:19:02 +0700

    add image for readme

--- lib/main.dart
+++ lib/main.dart
@@ -109,6 +109,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: "BukuBiruKu",
       debugShowCheckedModeBanner: false,
       home: AuthGate(),
     );
