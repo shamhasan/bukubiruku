a48a41ad406765afaf6d5220ef362ada122dfa4a
shamhasan <ihtishamulhasaaan@gmail.com>
2026-01-17 20:58:21 +0700

    add: db initialization at injection container, transactionlocaldatasource file

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -9,6 +9,8 @@ import 'package:money_tracker_app/transaction/domain/use_case/get_transactions.d
 import 'package:money_tracker_app/transaction/domain/use_case/scan_receipt.dart';
 import 'package:money_tracker_app/transaction/domain/use_case/update_transaction.dart';
 import 'package:money_tracker_app/transaction/presentation/providers/transaction_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 final sl = GetIt.instance;
 
@@ -44,4 +46,28 @@ Future<void> init() async {
       scannerService: sl(),
     ),
   );
+
+  final dbPath = await getDatabasesPath();
+  final path = join(dbPath, "bukubiruku.db");
+
+  final database = await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''
+       create table public.transactions (
+          id uuid not null default gen_random_uuid (),
+          user_id uuid not null,
+          type text not null,
+          amount numeric not null,
+          description text not null,
+          category text not null,
+          transaction_date timestamp with time zone null default now(),
+          constraint transactions_pkey primary key (id),
+          constraint transactions_user_id_fkey foreign KEY (user_id) references auth.users (id),
+          constraint transactions_type_check check (
+            (
+              type = any (array['income'::text, 'expense'::text])
+            )
+          )
+        ) TABLESPACE pg_default;
+    ''');
+    },);
 }
---
+++ lib/transaction/data/datasource/transaction_local_datasource.dart
@@ -0,0 +1,74 @@
+import 'dart:developer';
+
+import 'package:sqflite/sqflite.dart';
+
+import 'package:money_tracker_app/transaction/data/entities/transaction_model.dart';
+import 'package:uuid/uuid.dart';
+
+abstract class TransactionLocalDatasource {
+  Future<List<TransactionModel>> getTransaction(String userId);
+  Future<bool> addTransaction(TransactionModel transaction);
+  Future<void> updateTransaction(TransactionModel transaction);
+  Future<void> deleteTransaction(String id);
+}
+
+class TransactionLocalDatasourceImpl implements TransactionLocalDatasource {
+  final Database database;
+  TransactionLocalDatasourceImpl({required this.database});
+
+  @override
+  Future<bool> addTransaction(TransactionModel transaction) async {
+    try {
+      final data = transaction.toJson();
+      if (data['id'] == null || data['id'] == "") {
+        data['id'] = const Uuid().v4();
+      }
+      await database.insert(
+        "transactions",
+        transaction.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+      return true;
+    } catch (e) {
+      log("error insert: $e", name: 'TransactionLocalDatasource', error: e);
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> deleteTransaction(String id) async {
+    try {
+      await database.delete("transactions", where: 'id = ?', whereArgs: [id]);
+    } catch (e) {
+      log("error delete: $e", name: 'TransactionLocalDatasource', error: e);
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<TransactionModel>> getTransaction(String userId) async {
+    try {
+      final List<Map<String, dynamic>> maps = await database.query(
+        "transactions",
+      );
+      return maps.map((e) => TransactionModel.fromJson(e)).toList();
+    } catch (e) {
+      log("error get: $e", name: 'TransactionLocalDatasource', error: e);
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> updateTransaction(TransactionModel transaction) async {
+    try {
+      await database.update(
+        "transactions",
+        transaction.toJson(),
+        where: "${transaction.id} = ?",
+        whereArgs: [transaction.id],
+      );
+    } catch (e) {
+      log("error update: $e", name: 'TransactionLocalDatasource', error: e);
+    }
+  }
+}
